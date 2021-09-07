76d830502859f2df8e8483db0d0322bcb643ed93
Vinita Silaparasetty <vinitasilaparasetty@gmail.com>
2021-09-07 14:52:46 +0100

    Update skincancer.py

--- skincancer.py
+++ skincancer.py
@@ -69,7 +69,7 @@ def load_models():
 		url = 'https://github.com/VinitaSilaparasetty/dissertation/releases/download/maiden/skincancer_98.h5'
 		filename = url.split('/')[-1]
 		f_checkpoint=urllib.request.urlretrieve(url, filename)
-	model = load_model(f_checkpoint)
+	model = tf.keras.models.load_model(f_checkpoint)
 	return model
 
 
